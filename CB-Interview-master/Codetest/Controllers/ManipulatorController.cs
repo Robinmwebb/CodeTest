0c461bbf256b4e5471802aae833404ba70dc4d09
Robin Webb <robin.webb@cloudbooking.com>
2020-05-26 13:50:20 +0100

    initial commit

---
+++ CB-Interview-master/Codetest/Controllers/ManipulatorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Codetest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ManipulatorController : ControllerBase
+    {
+       
+
+        private readonly ILogger<ManipulatorController> _logger;
+
+        public ManipulatorController(ILogger<ManipulatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ManipulatorModel> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ManipulatorModel()
+            {
+                Message = $"Test Message {index}"              
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ CB-Interview-master/Codetest/ManipulatorModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Codetest
+{
+    public class ManipulatorModel    {
+
+        public string Message { get; set; }
+    }
+}
